cf7da77d27cc9e2a9b905eb5c9b71b9eac9c600a
IRAQ180 <saetyt12@gmail.com>
2026-07-07 18:30:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import asyncio
 from handlers import admin, cleaning, protection
 
 async def main():
-    bot = Bot(token="8201679973:AAFa6xGpxL7PxXX3s1QbNEXkMjy5Ah6kvcM")
+    bot = Bot(token="8201679973:AAFa6xGpxL7PxXX3s1QbNEXkMjy5Ah6kvcM") # ضع التوكن الجديد
     dp = Dispatcher()
     dp.include_routers(admin.router, cleaning.router, protection.router)
+    print("البوت يعمل الآن...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
