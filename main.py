ed2d117f2124e1f932f8d06fae0d0ea46bb9ea25
IRAQ180 <saetyt12@gmail.com>
2026-07-07 18:23:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from aiogram import Bot, Dispatcher
 import asyncio
-from handlers import info, admin, protection, cleaning
-
-# إعداد البوت
-# ضع التوكن الخاص ببوتك بين علامتي التنصيص بدلاً من النص الموجود
-TOKEN = "8201679973:AAFa6xGpxL7PxXX3s1QbNEXkMjy5Ah6kvcM"
+from handlers import admin, cleaning, protection
 
 async def main():
-    bot = Bot(token=TOKEN)
+    bot = Bot(token="8201679973:AAFa6xGpxL7PxXX3s1QbNEXkMjy5Ah6kvcM")
     dp = Dispatcher()
-    
-    # تسجيل جميع ملفات الأوامر (Handlers)
-    dp.include_router(info.router)
-    dp.include_router(admin.router)
-    dp.include_router(protection.router)
-    dp.include_router(cleaning.router)
-    
-    print("البوت يعمل الآن بنجاح...")
+    dp.include_routers(admin.router, cleaning.router, protection.router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    # تشغيل البوت
     asyncio.run(main())
