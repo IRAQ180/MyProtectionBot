1ffc390533a9b4b6581f56fa8df2fe5bfd2e187d
IRAQ180 <saetyt12@gmail.com>
2026-07-07 19:06:44 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from database import set_rank, get_rank, get_rank_level
+from database import set_rank, remove_rank, get_rank, get_rank_level
 
 router = Router()
 MY_ID = 8183727038
@@ -9,7 +9,7 @@ MY_ID = 8183727038
 async def promote(message: Message):
     if not message.reply_to_message: return await message.reply("يرجى الرد على العضو!")
     if message.from_user.id != MY_ID and get_rank_level(get_rank(message.from_user.id)) < 7:
-        return await message.reply("عذراً، هذا الأمر للمطورين فقط!")
+        return await message.reply("عذراً، هذا للمطورين فقط!")
     rank = message.text.replace("رفع ", "")
     set_rank(message.reply_to_message.from_user.id, rank)
-    await message.reply(f"تم رفع العضو إلى [{rank}] بنجاح.")
+    await message.reply(f"تم رفع العضو إلى [{rank}]")
