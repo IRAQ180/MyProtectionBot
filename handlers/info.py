d26f1984d61e40f9204009c1ca7bce7d98c1ab0b
IRAQ180 <saetyt12@gmail.com>
2026-07-07 19:05:00 +0300

    Update info.py

--- handlers/info.py
+++ handlers/info.py
@@ -4,28 +4,17 @@ from database import get_rank
 
 router = Router()
 
-@router.message(F.text == "آيدي") # تأكد من كتابة كلمة "آيدي" هنا
+@router.message(F.text == "ايدي")
 async def get_user_info(message: Message):
-    # تحديد المستخدم (الذي تم الرد عليه أو المستخدم نفسه)
     user = message.reply_to_message.from_user if message.reply_to_message else message.from_user
     rank = get_rank(user.id)
-    
-    # تحديد رتبة المطور الأساسي
-    if user.id == 8183727038: 
+    if user.id == 8183727038:
         rank = "المطور الأساسي"
-
-    info_text = (
+    
+    text = (
         f"☆-user : @{user.username or 'لا يوجد'}\n"
         f"☆-id : {user.id}\n"
         f"☆-الاسم : {user.full_name}\n"
         f"☆-الرتبة : {rank}"
     )
-    
-    try:
-        photos = await message.bot.get_user_profile_photos(user_id=user.id, limit=1)
-        if photos.total_count > 0:
-            await message.reply_photo(photo=photos.photos[0][0].file_id, caption=info_text)
-        else:
-            await message.reply(info_text)
-    except:
-        await message.reply(info_text)
+    await message.reply(text)
