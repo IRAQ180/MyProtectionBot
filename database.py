ebaa76cfb6f6acaaadea43322a3d893d6e831986
IRAQ180 <saetyt12@gmail.com>
2026-07-07 18:21:57 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('ranks.db', check_same_thread=False)
 cursor = conn.cursor()
-
 cursor.execute('CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, rank TEXT)')
 conn.commit()
 
@@ -20,8 +19,5 @@ def get_rank(user_id):
     return result[0] if result else "عضو"
 
 def get_rank_level(rank):
-    levels = {
-        "المطور الأساسي": 8, "مطور": 7, "مطور ثانوي": 6, "مالك": 5,
-        "منشئ اساسي": 4, "منشئ": 3, "مدير": 2, "مميز": 1, "عضو": 0
-    }
+    levels = {"المطور الأساسي": 8, "مطور": 7, "مطور ثانوي": 6, "مالك": 5, "منشئ اساسي": 4, "منشئ": 3, "مدير": 2, "مميز": 1, "عضو": 0}
     return levels.get(rank, 0)
